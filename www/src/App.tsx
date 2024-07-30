26b8f4c643045eb56076b4a42a39730143ba0fab
Leenard <lolleenard0@gmail.com>
2024-07-30 15:29:50 +0800

    feat: landing-inti

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,39 +1,72 @@
+import { SectionBody, SectionHeaderTitle, SuperHeader } from "./Head";
+
 function App() {
   return (
-    <div className="h-screen w-full flex flex-col gap-5 items-center justify-start mt-20">
-      <h1 className="text-4xl text-center mt-10 font-bold">Hello Leenard!</h1>
-      <p className="text-center mt-5">
-        Edit <code>App.tsx</code> and save to test HMR updates.
-      </p>
-      <p className="text-center mt-2">
-        <a
-          className="text-blue-500"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-        {' | '}
-        <a
-          className="text-blue-500"
-          href="https://reactjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React Docs
-        </a>
-        {' | '}
-        <a
-          className="text-blue-500"
-          href="https://github.com/leenrd/starter-cli"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Template Source
-        </a>
-      </p>
-    </div>
+    <main className="my-10 mx-auto max-w-[1120px]">
+      <SuperHeader />
+
+      <section>
+        <SectionHeaderTitle>Installation</SectionHeaderTitle>
+        <SectionBody>
+          Install worker using your package manager of your choice.
+          <pre className="my-3 bg-black/10 px-4 py-2 rounded-md">
+            npx worker-lnrd
+          </pre>
+        </SectionBody>
+      </section>
+
+      <br />
+
+      <section>
+        <SectionHeaderTitle>Initialize</SectionHeaderTitle>
+        <SectionBody>
+          Install worker using your package manager of your choice.
+          <pre className="my-3 bg-black/10 px-4 py-2 rounded-md">
+            npx worker-lnrd
+          </pre>
+        </SectionBody>
+      </section>
+
+      <br />
+
+      <section>
+        <SectionHeaderTitle>Usage</SectionHeaderTitle>
+        <SectionBody>
+          Install worker using your package manager of your choice.
+          <pre className="my-3 bg-black/10 px-4 py-2 rounded-md">
+            npx worker-lnrd
+          </pre>
+        </SectionBody>
+      </section>
+
+      <br />
+
+      <section>
+        <SectionHeaderTitle>More Examples</SectionHeaderTitle>
+        <SectionBody>
+          Install worker using your package manager of your choice.
+          <pre className="my-3 bg-black/10 px-4 py-2 rounded-md">
+            npx worker-lnrd
+          </pre>
+        </SectionBody>
+      </section>
+
+      <footer>
+        <br />
+        <br />
+        <br />
+        <p className="my-10 text-center text-gray-500">
+          &copy; {new Date().getFullYear()} Leenard Zarate. Check on{" "}
+          <a
+            href="https://github.com/leenrd/shein-worker-gg"
+            target="_blank"
+            className="underline text-blue-600"
+          >
+            github
+          </a>{" "}
+        </p>
+      </footer>
+    </main>
   );
 }
 
---
+++ www/src/Head.tsx
@@ -0,0 +1,55 @@
+import { Github, User2 } from "lucide-react";
+import { ReactNode } from "react";
+
+const SuperHeader = () => {
+  return (
+    <header className="my-12 flex items-start justify-between">
+      <div>
+        <h1 className="mb-4 tracking-tight leading-5 text-3xl font-semibold">
+          Worker API for Express.js
+        </h1>
+        <span>Leenard Zarate</span>
+      </div>
+      <div className="flex items-start justify-between gap-4">
+        <span>
+          <a href="https://github.com/leenrd/shein-worker-gg" target="_blank">
+            <Github className="cursor-pointer" />
+          </a>
+        </span>
+        <span>
+          <a href="https://leenard.tech/" target="_blank">
+            <User2 className="cursor-pointer" />
+          </a>
+        </span>
+      </div>
+    </header>
+  );
+};
+
+const SectionHeader = ({ children }: { children: ReactNode }) => {
+  return <header className="my-12">{children}</header>;
+};
+
+const SectionHeaderTitle = ({ children }: { children: ReactNode }) => {
+  return (
+    <h1 className="mb-4 tracking-tight leading-5 text-xl font-semibold">
+      {children}
+    </h1>
+  );
+};
+
+const SectionHeaderLabel = ({ children }: { children: ReactNode }) => {
+  return <span>{children}</span>;
+};
+
+const SectionBody = ({ children }: { children: ReactNode }) => {
+  return <p className="my-7">{children}</p>;
+};
+
+export {
+  SuperHeader,
+  SectionHeader,
+  SectionHeaderTitle,
+  SectionHeaderLabel,
+  SectionBody,
+};
